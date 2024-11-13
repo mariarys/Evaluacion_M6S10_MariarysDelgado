30126768f091edc0ddac12f00103ceb373e021b5
mariarys <delgadomariarys@gmail.com>
2024-11-12 22:01:31 -0300

    Update admin.py

--- sitio_de_libros/libros/admin.py
+++ sitio_de_libros/libros/admin.py
@@ -1,8 +1,25 @@
-from django.contrib import admin
-from .models import Book  
+from django.db import models
 
-class BookAdmin(admin.ModelAdmin):
-    list_display = ('titulo', 'autor', 'valoracion', 'fecha_creacion', 'fecha_modificacion')
+class Book(models.Model):
+    # Atributos
+    titulo = models.CharField(max_length=100, null=False)
+    autor = models.CharField(max_length=50, null=False)
+    valoracion = models.IntegerField(help_text='Valoración entre 0 y 100')
+    
+    # Nuevos campos de fecha
+    fecha_creacion = models.DateTimeField(auto_now_add=True)  # Fecha de creación
+    fecha_modificacion = models.DateTimeField(auto_now=True)  # Fecha de modificación
 
-admin.site.register(Book, BookAdmin)
+    # Metadatos
+    class Meta:
+        verbose_name = "Libro"
+        verbose_name_plural = "Libros"
+        permissions = [
+            ("development", "Permiso como Desarrollador"),
+            ("scrum_master", "Permiso como Scrum Master"),
+            ("product_owner", "Permiso como Product Owner"),
+        ]
+
+    def __str__(self):
+        return self.titulo  
 
