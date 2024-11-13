96bc4273e12efcb2598ef4ada74c2c68b32f0c43
mariarys <delgadomariarys@gmail.com>
2024-11-12 22:02:37 -0300

    Update admin.py

--- sitio_de_libros/libros/admin.py
+++ sitio_de_libros/libros/admin.py
@@ -1,25 +1,12 @@
-from django.db import models
+from django.contrib import admin
+from .models import Book
 
-class Book(models.Model):
-    # Atributos
-    titulo = models.CharField(max_length=100, null=False)
-    autor = models.CharField(max_length=50, null=False)
-    valoracion = models.IntegerField(help_text='Valoración entre 0 y 100')
-    
-    # Nuevos campos de fecha
-    fecha_creacion = models.DateTimeField(auto_now_add=True)  # Fecha de creación
-    fecha_modificacion = models.DateTimeField(auto_now=True)  # Fecha de modificación
+class BookAdmin(admin.ModelAdmin):
+   
+    list_display = ('titulo', 'valoracion', 'fecha_creacion', 'fecha_modificacion')
 
-    # Metadatos
-    class Meta:
-        verbose_name = "Libro"
-        verbose_name_plural = "Libros"
-        permissions = [
-            ("development", "Permiso como Desarrollador"),
-            ("scrum_master", "Permiso como Scrum Master"),
-            ("product_owner", "Permiso como Product Owner"),
-        ]
+    readonly_fields = ('fecha_creacion', 'fecha_modificacion')
 
-    def __str__(self):
-        return self.titulo  
+    list_filter = ('valoracion', 'fecha_modificacion')
 
+    admin.site.register(Book, BookAdmin)
